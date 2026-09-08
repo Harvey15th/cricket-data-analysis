d076a4ff23c845e77c5a8a7b7b7194ddfff29864
Harvey Gregg <harvey.gregg07@gmail.com>
2026-09-08 20:26:02 +0100

    implementing bugfixes relating to the printing of elo's after train has ran

--- src/cricket_elo/__main__.py
+++ src/cricket_elo/__main__.py
@@ -1,9 +1,9 @@
-from typing import Dict
+
 import argparse
 from train import train
 from benchmark import benchmark
 from predict import predict
-MODE_CHOICES = ['train', 'verify', 'predict']
+MODE_CHOICES = ['train', 'predict', 'benchmark']
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -13,30 +13,30 @@ if __name__ == "__main__":
     parser.add_argument("-input_data", 
                         type=str, 
                         help='Path to the game data used for training or benchma',
-                        argument_default = None)
+                        default = '')
     parser.add_argument("-output_path",
                         type=str,
                         help='Path to the output file',
-                        argument_default = None)
+                        default = '')
     parser.add_argument("-input_data_verification", 
                         type=str, 
                         help="Path to the data file used for verification",
-                        argument_default = None)
+                        default = '')
     parser.add_argument("-team_1",
                         type=str,
                         help="Name of first team used for predict",
-                        argument_default=None)
+                        default='')
     parser.add_argument("-team_2",
                         type=str,
                         help="Name of second team used for predict",
-                        argument_default=None)
+                        default='')
     args = parser.parse_args()
 
     if args.mode == 'train':
         #Verification
-        if args.input_data is None:
+        if args.input_data is '':
             raise ValueError("Input data path must be provided for training mode.")
-        if args.output_path is None:
+        if args.output_path is '':
             raise ValueError("Output path must be provided for training mode.")
 
         #Training
@@ -47,11 +47,11 @@ if __name__ == "__main__":
 
     elif args.mode == 'benchmark':
         #Verification
-        if args.input_data is None:
+        if args.input_data is '':
             raise ValueError("Input data path must be provided for benchmarking")
-        if args.input_data_verification is None:
+        if args.input_data_verification is '':
             raise ValueError("input_data_verification must be provided for benchmarking")
-        if args.output_path is None:
+        if args.output_path is '':
             raise ValueError("Output path must be provided for benchmarking")
         
         #Benchmarking
@@ -62,19 +62,13 @@ if __name__ == "__main__":
     
     elif args.mode == 'predict':
         #Verification
-        if args.team_1 is None:
+        if args.team_1 is '':
             raise ValueError("Team 1 must be provided for prediction")
-        if args.team_2 is None:
+        if args.team_2 is '':
             raise ValueError("Team 2 must be provided for prediction")
-        if args.input_data is None:
+        if args.input_data is '':
             raise ValueError("Input data path must be provided for prediction")
 
         #Prediction
         if not predict(args.input_data, args.team_1, args.team_2):
             print("Prediction Unsuccessful")
-
-        
-
-    
-        
-
